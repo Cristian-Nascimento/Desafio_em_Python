8db2d6563c27aa2a8d068e68cc7e097d47d5d2ee
Cristian Nasicmento <93889204+Cristian-Nascimento@users.noreply.github.com>
2022-09-05 20:35:38 -0300

    Update Gradiente.py
    mudança do algoritmo

--- Gradiente.py
+++ Gradiente.py
@@ -13,110 +13,67 @@ Implementar o Algoritmo do Gradiente na orimização do seguinte problema:
 .Ponto inicial:  x¯0=[10,10]T
 '''
 
-from tkinter.ttk import Style
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  4 00:01:55 2022
+
+@author: Cristian
+"""
+
 import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
 
+n = 28
+grad = np.zeros(n)
+pre = 10**(-1)                                          #precisão
+
+dados = pd.DataFrame()
+dados['x'] = np.linspace(10,0,11)
+dados['y'] = np.linspace(10,0,11)
+x1 = dados['x']
+x2 = dados['y']
+
+def f_x(x1, x2):
+    """
+    Parameters
+    ----------
+    x1 (float): Ponto X1 da funcão no eixo x.
+    x2 (float): Ponto X2 da função no eixo y.
+
+    Returns
+    -------
+    f_x (float): Após o calculo, irá me retornar o vetor 'Z'
+    """
+    return (x1-1)**2 + 2*x2**2
 
 
-# dados = pd.DataFrame()
-# dados['x'] = np.linspace(10,0,100)   #Criação do vertor de x1
-# dados['y'] = np.linspace(10,0,100)   #Criação do vertor de x2
-# x1 = dados['x']
-# x2 = dados['y']
+def f_gradX(x1, x2):
+    """
+    Parameters
+    ----------
+    x1 (float): Ponto X1 da funcão no eixo x.
+    x2 (float): Ponto X2 da função no eixo y.
 
-# print(type(x1), type(x2))
+    Returns
+    -------
+    f_gradX (float): Após o calculo, irá me retornar o gradiente de 'Z'
+    """
+    return 2*(x1-1) + 4*x2                              #Derivada parcial em relação a x1 = 2*(x1-1) e em x2 = 4*x2
 
-# def f_x(x1, x2):
-#     """
-#     Parametro
-#     ----------
-#     x1 (float): Ponto X1 da funcão no eixo x.
-#     x2 (float): Ponto X2 da função no eixo y.
-
-#     Retorno
-#     ----------
-#     f_x (float): Após o calculo, irá me retornar o vetor 'Z'
-#     função que retorna o valor de f(x1,x2) para um deterninado x1,x2
-#     """
-#     return (x1-1)**2 + 2*x2**2
-
-# def linha_x1(x1):
-#     """
-#     Parametro
-#     ----------
-#     x1 (float): Ponto X1 da funcão custo no eixo x1.
+for i in range(n): 
     
-#     Retorno
-#     ----------
-#         Função que retorna o valor de custo de f(x1) para um deterninado x1
-#     """
-#     return 2*(x1 - 1)
+    grad[i] = (2*(i-1) + 4*i)/((2*(i-1))**2 + (4*i)**2)
+    print(f' o vetor dk de posição:{[i]} na função objetivo tem seu valor de: {grad[i]:.5f}\n')
+    
+def z_funcao():
+    return np.array(f_x(x1,x2))
 
-# def linha_x2(x2):
-#     """
-#     Parametro
-#     ----------
-#     x2 (float): Ponto X2 da funcão custo no eixo x2.
+dados['z'] = np.array(f_x(x1,x2))
+z_final = dados['z']
 
-#     Retorno
-#     ----------
-#         Função que retorna o valor de custo de f(x2) para um deterninado x2
-
-#     """
-#     return 4*x2
-
-# def z_funcao():
-#     """
-#     Parametro
-#     ----------
-#     Z (float): Ponto Z da funcão custo no eixo z.
-
-#     Retorno
-#     ----------
-#         Função que retorna um array para o Z do valor de custo de f(x1, x2) para um deterninado x1, x2
-
-#     """
-#     return np.array(f_x(x1,x2))
-
-# dados['z'] = np.array(f_x(x1,x2))
-# z_final = dados['z']   #z_final está recebendo o array de Z
-
-# print(f' A funcao tem o seu valor de:\n{f_x(x1, x2)}\n')
-# print(f' A funcao custo em X1 tem o seu valor de:\n{linha_x1(x1)} \ne em X2: \n{linha_x2(x2)}')
-
-# grafico = plt.axes(projection='3d')
-# grafico.plot(x1,x2,f_x(x1,x2))    #plotagem da curvas de nível da funcão custo(objetivo)
-# grafico.scatter([1],[0], color='black', marker='x')    #marcação do ponto ótimo [1,0]
-
-# grafico.set(title='Curvas de Nível Gradiente', xlabel='eixo x1', ylabel='eixo x2', zlabel='eixo f(x1,x2)')
-# plt.show()
-
-a = []
-b = []
-k = 10
-
-while k >= 0:
-    a.append(int(k)) #Criação do vertor de x1
-    k += -1
-
-grad = []
-dk = []
-
-def inf(a):
-    return (2*(a[i]-1) + 4*a[i])
-
-for i in range(len(a)):
-    grad.append(((2*(a[i]-1))**2 + (4*a[i])**2)) # parte de baixo
-
-
-
-print(inf(a))
-# print(type(a),type(grad))
-# grafico = plt.axes(projection='3d')
-# grafico.plot(a,grad)    #plotagem da curvas de nível da funcão custo(objetivo)
-# grafico.scatter([1],[0], color='black', marker='x')    #marcação do ponto ótimo [1,0]
-
-# grafico.set(title='Curvas de Nível Gradiente', xlabel='eixo x1', ylabel='eixo x2', zlabel='eixo f(x1,x2)')
-# plt.show()
+grafico = plt.axes(projection='3d')
+grafico.plot( x1, x2, z_final)                          #plotagem da curvas de nível da funcão custo(objetivo)
+grafico.scatter([1],[0], color='black', marker='x')     #marcação do ponto ótimo [1,0]
+grafico.set(title='Curvas de Nível Gradiente', xlabel='eixo x1', ylabel='eixo x2', zlabel='f(x1,x2)')
+plt.show()
