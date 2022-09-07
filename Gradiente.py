59f7890689a7e107ab22a278061bca07b6e2ff3b
Cristian Nasicmento <93889204+Cristian-Nascimento@users.noreply.github.com>
2022-09-06 22:11:38 -0300

    Update Gradiente.py
    Versão Final

--- Gradiente.py
+++ Gradiente.py
@@ -13,7 +13,6 @@ Implementar o Algoritmo do Gradiente na orimização do seguinte problema:
 .Ponto inicial:  x¯0=[10,10]T
 '''
 
-# -*- coding: utf-8 -*-
 """
 Created on Sun Sep  4 00:01:55 2022
 
@@ -24,61 +23,36 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
 
-n = 100
-grad = np.zeros(n)
-pre = float(0.01)                                                  #precisão
-
 dados = pd.DataFrame()
-dados['x'] = np.linspace(10,0,11)
-dados['y'] = np.linspace(10,0,11)
-x1 = dados['x']
-x2 = dados['y']
+dados['x1'] = np.linspace(10,-10,21)
+dados['x2'] = np.linspace(10,-10,21)
+x1 = dados['x1']
+x2 = dados['x2']
+repeticao = 0
+norma_grad = 0
+pre = pow(10,-2)
 
-def f_x(x1, x2):                                                #função que retorne o valor de  f(x¯)
-    """
-    Parameters
-    ----------
-    x1 (float): Ponto X1 da funcão no eixo x.
-    x2 (float): Ponto X2 da função no eixo y.
-
-    Returns
-    -------
-    f_x (float): Após o calculo, irá me retornar o vetor 'Z'
-    """
+def f_x(x1, x2):
     return (x1-1)**2 + 2*x2**2
 
+xx1,xx2 = np.meshgrid(x1,x2)
+zlinha = f_x(xx1, xx2)
 
-def f_gradX(x1, x2):                                            #função que retorne o valor da derivada parcial em x1 e x2 da função: f(x¯)
-    """
-    Parameters
-    ----------
-    x1 (float): Ponto X1 da funcão no eixo x.
-    x2 (float): Ponto X2 da função no eixo y.
+def f_gradX(x1, x2):
+    return pd.Series([[2*(x1-1)],[4*x2]])
 
-    Returns
-    -------
-    f_gradX (float): Após o calculo, irá me retornar o gradiente de 'Z'
-    """
-    return 2*(x1-1) + 4*x2                                      #Derivada parcial em relação a x1 = 2*(x1-1) e em x2 = 4*x2
-
-for i in range(n): 
-    grad[i] = (2*(i-1) + 4*i)/((2*(i-1))**2 + (4*i)**2)         #Construtor de pontos de x1 e x2 no Dk
-    print(f' o vetor dk de posição:{[i]} na função objetivo tem seu valor de: {grad[i]:.5f}\n')
-    
-    if grad[i] >= pre:
-        pass
-        
-        if grad[i] < pre:
+for i in dados['x1']:
+    for j in dados['x2']:
+        if norma_grad >= pre and norma_grad <= pre or repeticao >= 100 :
             break
-
-def z_funcao():
-    return np.array(f_x(x1,x2))
-
-dados['z'] = np.array(f_x(x1,x2))
-z_final = dados['z']
+        funcao_x1x2 = (i-1)**2 + 2*j**2
+        print(f' o vetor dk de posição: x1 ={[i]} e x2={[j]} na função objetivo tem seu valor de: {funcao_x1x2:.2f}\n')
+        norma_grad = pow(2*(i-1),2) + pow(4*j,2)
+        
+        repeticao = repeticao + 1
 
 grafico = plt.axes(projection='3d')
-grafico.plot( x1, x2, z_final)                                  #plotagem da curvas de nível da funcão custo(objetivo)
-grafico.scatter([1],[0], color='black', marker='x')             #marcação do ponto ótimo [1,0]
+grafico.contour( xx1, xx2, zlinha)
+grafico.scatter([1],[0], color='black', marker='x')
 grafico.set(title='Curvas de Nível Gradiente', xlabel='eixo x1', ylabel='eixo x2', zlabel='f(x1,x2)')
 plt.show()
